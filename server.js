7815d4af2064f6926fd0af85648d0d979ea1cc18
Tabatha Bell <tabathambell@live.com>
2021-01-17 19:56:40 -0500

    finished lesson 3

--- server.js
+++ server.js
@@ -55,7 +55,12 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // Delete a candidate
 app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.run(sql, params, function(err, result) {
       if (err) {
@@ -77,8 +82,11 @@ app.post('/api/candidate', ({ body }, res) => {
       res.status(400).json({ error: errors });
       return;
     }
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
-              VALUES (?,?,?)`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
     const params = [body.first_name, body.last_name, body.industry_connected];
     // ES5 function, not arrow function, to use `this`
     db.run(sql, params, function(err, result) {
